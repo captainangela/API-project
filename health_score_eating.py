838dd6c636024f8d3c584b53310b55bb8d2773d4
angela lui <angelalui@angelas-MacBook-Air.local>
2017-03-19 14:16:01 -0700

    almost done

--- health_score_eating.py
+++ health_score_eating.py
@@ -1,34 +1,78 @@
+from flask import Flask, request, render_template
 from urllib2 import urlopen
 from json import load
 from collections import defaultdict
 from random import sample
 
-#sf open data source: live health scores
-apiUrl = "http://data.sfgov.org/api/views/pyih-qa8i/rows.json?"
+app = Flask(__name__)
 
-#open the apiUrl and assign data to variable
+apiUrl = "http://data.sfgov.org/api/views/pyih-qa8i/rows.json?"
 response = urlopen(apiUrl)
 json_obj = load(response)
 data = json_obj['data']
 
-def rest_name(r): 
-	return r[9]
+@app.route("/")
+def index():
+    return """
+    <html>
+      <body>
+        <h1>Welcome!</h1>
+        Click here to check for restaurants!
+      </body>
+    </html>
+    """    
+
+@app.route('/health')
+def enter_zip_code():    
+    return """
+    <html>
+    <head>
+    <title>Health Scores</title>
+    </head>
+    <body>
+        <form action='scores'>
+            What zip code will you be dining in? 
+            <input type ='text' name ='zip_code'>
+            <input type ='submit'>
+        </form>
+    </body>
+    </html> 
+    """
+
+@app.route('/scores')
+def get_health_scores():
+    zip_code = request.args.get('zip_code')
+    
+    restaurants = {}
+    for row in data:
+        restaurants[rest_name(row)] = row
+
+    zips = defaultdict(list)
+    for rest in restaurants.values():
+        zip = rest[13]
+        zips[zip].append(rest)
+
+    rest_list = []
+    for rest in sample(zips[str(zip_code)], 10):
+        rest_list.append(rest_name(rest).title())
+        risk = get_rating(rest)
+
+    return render_template('health_score.html', restaurants = rest_list, zippy = zip_code)
+    
+def rest_name(name): 
+    return name[9]
+
+def get_rating(r):
+    return r[-1]
 
 #get rid of duplicate rows
-restaurants = {}
-for row in data:
-    # the last row has the latest health inspection
-    restaurants[rest_name(row)] = row
+
+
+#get the health rating
+ratings = {}
 
 #organize by zip code
-zips = defaultdict(list)
-for rest in restaurants.values():
-	zip = rest[13]
-	zips[zip].append(rest)
 
-zip_code = raw_input("What San Francisco zip code will you be dining in today? ")
 
-print "Here are 10 restaurants in your area: "
-for rest in sample(zips[str(zip_code)], 10):
-	print rest_name(rest)
-
+if __name__ == "__main__":
+    app.run(debug=True) 
